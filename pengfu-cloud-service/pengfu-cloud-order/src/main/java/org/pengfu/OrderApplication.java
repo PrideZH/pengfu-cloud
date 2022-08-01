452178ac46dd672d264fa69165d2aaf05f716809
PrideZH <332842890@qq.com>
2022-08-01 14:40:01 +0800

    🐛 添加@EnableFeignClients注解对FeignClient进行扫描

--- pengfu-cloud-service/pengfu-cloud-order/src/main/java/org/pengfu/OrderApplication.java
+++ pengfu-cloud-service/pengfu-cloud-order/src/main/java/org/pengfu/OrderApplication.java
@@ -2,12 +2,14 @@ package org.pengfu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author PrideZH <332842890@qq.com>
  * @date 2022/7/27 18:43
  */
 @SpringBootApplication
+@EnableFeignClients
 public class OrderApplication {
 
     public static void main(String[] args) {
--- pengfu-cloud-service/pengfu-cloud-user/src/main/java/org/pengfu/UserApplication.java
+++ pengfu-cloud-service/pengfu-cloud-user/src/main/java/org/pengfu/UserApplication.java
@@ -2,12 +2,14 @@ package org.pengfu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author PrideZH <332842890@qq.com>
  * @date 2022/7/27 15:39
  */
 @SpringBootApplication
+@EnableFeignClients
 public class UserApplication {
 
     public static void main(String[] args) {
